79f6c4abc44091e14947bee4e9e935d187852770
firetdev <futuregamesstore@gmail.com>
2026-03-05 20:47:44 -0600

    Updated backend

--- backend/src/get-colors.ts
+++ backend/src/get-colors.ts
@@ -11,19 +11,19 @@ function distance(c1: Color, c2: Color): number {
 
 export function getColors(clrs: Color[], num: number): Color[] {
   let centers: Color[] = [];
-  let oldCenters: Color[] = [];
-  let groups: Color[][] = [];
+  
   for (let i = 0; i < num; i += 1) {
     let a = Math.floor(Math.random() * clrs.length);
-    let newColor = clrs[a];
-    for (let e = 0; e < centers.length; e++) {
-      while (isSimilar(centers[e], newColor)) {
-        a = Math.floor(Math.random() * clrs.length);
-        newColor = clrs[a];
-      }
+    let newColor = { ...clrs[a] };
+    
+    let attempts = 0;
+    while (centers.some(c => isSimilar(c, newColor)) && attempts < 10) {
+      a = Math.floor(Math.random() * clrs.length);
+      newColor = { ...clrs[a] };
+      attempts++;
     }
+    
     centers.push(newColor);
-    groups.push([]);
   }
 
   let iterations = 0;
@@ -31,51 +31,54 @@ export function getColors(clrs: Color[], num: number): Color[] {
 
   while (iterations < maxIterations) {
     iterations++;
-    groups = centers.map(() => []);
+    
+    const groups: Color[][] = centers.map(() => []);
 
-    for (let u = 0; u < 3; u += 1) {
-      for (let i = 0; i < clrs.length; i += 1) {
-        let prevDist = 10000000;
-        let point = 0;  // Index of center point which color is closest to
+    for (let i = 0; i < clrs.length; i += 1) {
+      let prevDist = Infinity;
+      let point = 0;
 
-        for (let e = 0; e < centers.length; e += 1) {
-          const dist = distance(centers[e], clrs[i]);
-
-          if (dist < prevDist) {
-            prevDist = dist;
-            point = e;
-          }
-        }
-
-        groups[point].push(clrs[i]);
-      }
-
-      for (let i = 0; i < centers.length; i += 1) {
-        const totals = {
-          r: 0,
-          g: 0,
-          b: 0
-        };
-
-        for (let e = 0; e < groups[i].length; e += 1) {
-          totals.r += groups[i][e].r;
-          totals.g += groups[i][e].g;
-          totals.b += groups[i][e].b;
-        }
-
-        oldCenters = centers.map(c => ({ ...c }));
-
-        if (groups[i].length > 0) {
-          centers[i].r = totals.r / groups[i].length;
-          centers[i].g = totals.g / groups[i].length;
-          centers[i].b = totals.b / groups[i].length;
-        }
-        for (let e = 0; e < centers.length; e += 1) {
-          if (Math.abs(centers[i].r - oldCenters[i].r) > 1 || Math.abs(centers[i].g - oldCenters[i].g) > 1 || Math.abs(centers[i].b - oldCenters[i].b) > 1)
-            break;
+      for (let e = 0; e < centers.length; e += 1) {
+        const dist = distance(centers[e], clrs[i]);
+        if (dist < prevDist) {
+          prevDist = dist;
+          point = e;
         }
       }
+      groups[point].push(clrs[i]);
     }
+
+    let hasMoved = false;
+
+    for (let i = 0; i < centers.length; i += 1) {
+      if (groups[i].length === 0) continue;
+
+      let totals = { r: 0, g: 0, b: 0 };
+      for (let e = 0; e < groups[i].length; e += 1) {
+        totals.r += groups[i][e].r;
+        totals.g += groups[i][e].g;
+        totals.b += groups[i][e].b;
+      }
+
+      const newR = totals.r / groups[i].length;
+      const newG = totals.g / groups[i].length;
+      const newB = totals.b / groups[i].length;
+
+      if (
+        Math.abs(centers[i].r - newR) > 1 || 
+        Math.abs(centers[i].g - newG) > 1 || 
+        Math.abs(centers[i].b - newB) > 1
+      ) {
+        hasMoved = true;
+      }
+
+      centers[i].r = newR;
+      centers[i].g = newG;
+      centers[i].b = newB;
+    }
+
+    if (!hasMoved)
+      break; 
   }
 
   return centers;
